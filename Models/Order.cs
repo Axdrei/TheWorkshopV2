e282880f2c424f0458f48369c8fd0aa0b276c982
Andrei <Andrei@DESKTOP-LK2IIP3>
2021-12-16 02:34:28 -0500

    Add project files.

---
+++ Models/Order.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheWorkshopV2.Models
+{
+    public class Order
+    {
+        //Creates a OrderID int with annotations declaring it the primary Key, and editing the DisplayName
+        [Key]
+        [DisplayName("Order ID")]
+        public int OrderID { get; set; }
+
+        //Creates a customerID string with annotations declaring it to be a foreign key from object IdentityUser and editing the DisplayName
+        [DisplayName("Customer ID")]
+        public string CustomerID { get; set; }
+        [ForeignKey("CustomerID")]
+        public IdentityUser User { get; set; }
+
+        //Creates a ProductID int with annotations declaring it to be a foreign key from object Product and editing the DisplayName
+        [DisplayName("Product ID")]
+        public int ProductID { get; set; }
+        [ForeignKey("ProductID")]
+        public Product Product { get; set; }
+
+        //Creates a DateTime DatePurchased which will store the date the product was purchased
+        [DisplayName("Date Purchased")]
+        public DateTime DatePurchased { get; set; }
+
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheWorkshopV2.Models
+{
+    public class Product
+    {
+        //Creates primary key Product ID, and edits the Display name
+        [DisplayName("Product ID")]
+        public int productID { get; set; }
+
+        //Creates Product Image -- Image Name, and edits the Display name
+        [DisplayName("Image Name")]
+        public string productImage { get; set; }
+
+        //Creates Product name string, and edits the Display name
+        [DisplayName("Product Name")]
+        public string productName { get; set; }
+
+        //Creates Product Description string, and edits the Display name
+        [DisplayName("Product Description")]
+        public string productDesc { get; set; }
+
+        //Creates Product Price double, and edits the Display name
+        [DisplayName("Product Price")]
+        public double productPrice { get; set; }
+
+        //Creates a non-mapped IFormFile ImageFile -- Meaning it will not be in the database
+        //IFormFile lets you store files such as images
+        [NotMapped]
+        [DisplayName("Product Picture")]
+        public IFormFile ImageFile { get; set; }
+    }
+}
